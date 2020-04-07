42edfc522197f5d89f35f4a786066d6054f6f2a0
Breakermoob <breakermoob@hotmail.com>
2020-04-07 11:31:01 -0500

    Documentación del juego

--- car_racing.py
+++ car_racing.py
@@ -41,7 +41,7 @@ car2Img = pygame.image.load("assets/car2.png")  # cargamos los carros del juego
 bgImg = pygame.image.load("assets/back2.jpg")  #Cargamos la pista de juego
 bgImg2 = pygame.image.load("assets/back3.jpg") #Cargamos la pista de juego
 crash_img = pygame.image.load("assets/crash.png")  # Imagen de crash
-background = pygame.image.load("assets/background.jpg")  # Imagen de crash
+background = pygame.image.load("assets/Background.jpg")  # Imagen de crash
 cap = cv2.VideoCapture(0) # Es la camara por defecto del ordenador.
 cordeX, cordeY = 0, 0 #Coordenadas del objeto a detectar por su color
 car_x_change = 0 #Dirección en la que se mueve el vehiculo deacuerdo a la pocision donde se encuentre
@@ -125,215 +125,224 @@ def openCamera():
 #-----------------------------------------------------------------------------------------------------------
 
 
-
+#El siguiente método se renderiza la ventana inicial del juego, la cúal contiene el menú.
 def intro():
-    pygame.mixer.Sound.play(intro_sound)
-    intro_sound.set_volume(0.1)
-    intro = True
+    pygame.mixer.Sound.play(intro_sound) #Iniciamos la música
+    intro_sound.set_volume(0.1) #Control de volumen
+    intro = True #variable para identificar que debe renderizar el menu
+    #Variables para los botones del menu
     menu1_x = 200
     menu1_y = 400
     menu2_x = 500
     menu2_y = 400
     menu_width = 100
     menu_height = 50
+
+    #Ciclo para mantener abierto el menu
     while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get(): #Listener de pygame para eventos
+            if event.type == pygame.QUIT: #Opción para salir
                 pygame.quit()
                 quit()
-        pygame.display.set_icon(carImg)
+        pygame.display.set_icon(carImg) #Icono del juego
 
-        gameDisplay.blit(background, (0, 0))
-        pygame.draw.rect(gameDisplay, green, (200, 400, 100, 50))
-        pygame.draw.rect(gameDisplay, red, (500, 400, 100, 50))
+        gameDisplay.blit(background, (0, 0)) #Imagen de fondo 
+        pygame.draw.rect(gameDisplay, green, (200, 400, 100, 50)) #Rectangulo boton
+        pygame.draw.rect(gameDisplay, red, (500, 400, 100, 50)) #Rectangulo boton
 
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
+        mouse = pygame.mouse.get_pos() #listener mouse
+        click = pygame.mouse.get_pressed() #listener click
 
         if menu1_x < mouse[0] < menu1_x + menu_width and menu1_y < mouse[1] < menu1_y + menu_height:
             pygame.draw.rect(gameDisplay, blue, (200, 400, 100, 50))
-            if click[0] == 1:
+            if click[0] == 1: #Si ocurre este evento, iniciara el juego
                 pygame.mixer.Sound.stop(intro_sound)
                 intro = False
         if menu2_x < mouse[0] < menu2_x + menu_width and menu2_y < mouse[1] < menu2_y + menu_height:
             pygame.draw.rect(gameDisplay, blue, (500, 400, 100, 50))
-            if click[0] == 1:
+            if click[0] == 1: #Si ocurre este evento cerrara el juego
                 pygame.quit()
                 quit()
 
-        message_display("Go", 40, menu1_x + menu_width / 2, menu1_y + menu_height / 2)
-        message_display("Exit", 40, menu2_x + menu_width / 2, menu2_y + menu_height / 2)
-        pygame.display.update()
-        clock.tick(50)
+        message_display("Start", 40, menu1_x + menu_width / 2, menu1_y + menu_height / 2) #Texto del boton del juego
+        message_display("Exit", 40, menu2_x + menu_width / 2, menu2_y + menu_height / 2) #Texto del boton de salir
+        pygame.display.update() #Esta linea la veremos multiples veces, lo hace es renderizar el contenido e la ventana
+        clock.tick(50) #Control de frames
+
 # This function update the score
 def highscore(count):
-    font = pygame.font.SysFont(None, 60)
-    text = font.render("Score : " + str(count), True, black)
-    gameDisplay.blit(text, (0, 0))
+    font = pygame.font.SysFont(None, 60) #Tamaño de Fuente
+    text = font.render("Score : " + str(count), True, black)#Texto
+    gameDisplay.blit(text, (0, 0)) #Renderizado 
 
 
 def try_again_counter(count, score):
     # Try Again Timer
-    font = pygame.font.SysFont(None, 250)
-    text = font.render(str(count), True, black)
-    gameDisplay.blit(text, (350, 250))
+    font = pygame.font.SysFont(None, 250) #Tamaño de Fuente
+    text = font.render(str(count), True, black) #Texto
+    gameDisplay.blit(text, (350, 250)) #Renderizado
 
     # Final Score
-    font = pygame.font.SysFont(None, 115)
-    text = font.render("Score : " + str(score), True, black)
-    gameDisplay.blit(text, (165, 170))
+    font = pygame.font.SysFont(None, 115) #Tamaño de Fuente
+    text = font.render("Score : " + str(score), True, black) #Texto
+    gameDisplay.blit(text, (165, 170)) #Renderizado
 
 
 # This function print obstacle cars
 def draw_things(thingx, thingy, thing):
-    gameDisplay.blit(thing, (thingx, thingy))
+    gameDisplay.blit(thing, (thingx, thingy)) #Dibujamos los autos obstaculos en la via
 
 
 # This function print our car
 def car(x, y):
-    gameDisplay.blit(carImg, (x, y))
+    gameDisplay.blit(carImg, (x, y)) #Dibujamos el auto del usuario
 
 
 # this function make the rectangles for buttons from initial menu
-def text_objects(text, font):
+def text_objects(text, font):#Botones dle menu
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
 
 # This functio print buttons
-def message_display(text, size, x, y):
-    font = pygame.font.Font("assets/CaviarDreams.ttf", size)
-    text_surface, text_rectangle = text_objects(text, font)
-    text_rectangle.center = (x, y)
-    gameDisplay.blit(text_surface, text_rectangle)
+def message_display(text, size, x, y): #Esta funcion se utiliza para la mayoria de mensajes que muestra el juego
+    font = pygame.font.Font("assets/CaviarDreams.ttf", size)#Fuente
+    text_surface, text_rectangle = text_objects(text, font) #Texto
+    text_rectangle.center = (x, y) #Bloque
+    gameDisplay.blit(text_surface, text_rectangle) #render
 
-def crash(x,y,score):
-    pygame.mixer.Sound.play(crash_sound)
-    pygame.mixer.music.stop()
-    reset = 3
+def crash(x,y,score): #Funcion de choque
+    pygame.mixer.Sound.play(crash_sound) #Sonido de choque
+    pygame.mixer.music.stop()#Detenemos toda la musica
+    reset = 3 #contador
     #Stop Music
     #this put the crach img in the cash car position
-    gameDisplay.blit(crash_img,(x,y))
+    gameDisplay.blit(crash_img,(x,y)) #Imagen de choque
     # Put Message, update display, and wait
-    message_display("You Crashed",115,display_width/2,display_height/2)
-    pygame.display.update()
-    time.sleep(3)
+    message_display("You Crashed",115,display_width/2,display_height/2) #Mensaje de choque
+    pygame.display.update() #Actualizar vista
+    time.sleep(3)#Esperar
 
-    while reset >= 0:
-        gameDisplay.fill(white)
-        try_again_counter(reset,score)
+    while reset >= 0: #Ciclo para el contador despues de chocar
+        gameDisplay.fill(white)#fondo blanco
+        try_again_counter(reset,score) #Dipslay counter
         pygame.display.update()
         time.sleep(1)
         reset-=1
     time.sleep(0.5)
-    intro()
-    gameloop() #for restart the game
+    intro()#Abrir el menu de inicio
+    gameloop() #Reiniciar variables del juego
 
+#Funcion para el juego
 def gameloop():
-    global car_x_change
-    pygame.mixer.Sound.play(start_sound)
+    global car_x_change #Variable de cambio para el auto
+    pygame.mixer.Sound.play(start_sound) #Sonido de inicio
     pygame.mixer.music.stop()
-    pygame.mixer.music.load("assets/game.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.music.load("assets/game.mp3")#Musica de fondo
+    pygame.mixer.music.play(-1)#Musica de fondo
+    #Variables de inicio para el fondo
     bg_x1 = 0
     bg_x2 = 0
     bg_y1 = 0
     bg_y2 = -600
-    bg_speed = 6
-    bg_speed_change = 0
-    car_x = ((display_width / 2) - (car_width / 2))
-    car_y = (display_height - car_height)
-    car_x_change = 0
-    road_start_x = (display_width / 2) - 112
-    road_end_x = (display_width / 2) + 112
+    bg_speed = 6 #Velocidad inicial del fondo
+    bg_speed_change = 0 #Velocidad de cambio
+    car_x = ((display_width / 2) - (car_width / 2)) #Posición del auto
+    car_y = (display_height - car_height) #Posición del auto
+    car_x_change = 0 #Velocidad de cambio de los obstaculos
+    road_start_x = (display_width / 2) - 112 #Posición de la carretera
+    road_end_x = (display_width / 2) + 112 #Posición de la carretera
 
-    thing_startx = random.randrange(road_start_x, road_end_x - car_width)
-    thing_starty = -600
+    thing_startx = random.randrange(road_start_x, road_end_x - car_width) #Auto obstaculo random 
+    thing_starty = -600 #Posición inicial del obstaculo
+    #variables del estado del juego
     thingw = 50
     thingh = 100
-    thing_speed = 1
-    count = 0
-    pause = False
-    gameExit = False
+    thing_speed = 1 #obstaculos
+    count = 0 #Score
+    pause = False #pausa
+    gameExit = False #ejecutando
 
-    while not gameExit:
+    while not gameExit: #Ciclo de juego
 
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #Listene de eventos
 
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #Opcion para cerrarlo
                 gameExit = True
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN: #alternativas de control
                 # Here we can put the camera results
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT: #Movimiento a la izquierda
                     car_x_change = -1.5
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT: #Movimiento a la derecha
                     car_x_change = 1.5
-                elif event.key == pygame.K_SPACE:
+                elif event.key == pygame.K_SPACE: #Pausa
                     pause = True
 
-            if event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP: #Detenerse
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     car_x_change = 0
 
-        while pause == True:
-            message_display("Game Paused",115,display_width/2,display_height/2)
+        while pause == True: #Ciclo de pausa
+            pygame.mixer.music.pause() #pausar la musica
+            message_display("Game Paused",115,display_width/2,display_height/2) #Mostrar mensaje
             pygame.display.update()
-            time.sleep(0.5)
-            gameDisplay.blit(bgImg, (bg_x1, bg_y1))
-            gameDisplay.blit(bgImg2, (bg_x2, bg_y2))
-            car(car_x, car_y)  # display car
-            draw_things(thing_startx, thing_starty, car2Img)
-            highscore(count)
-            pygame.display.update()
-            time.sleep(0.5)
-            for event in pygame.event.get():
+            time.sleep(0.5) #Esperar
+            gameDisplay.blit(bgImg, (bg_x1, bg_y1)) #Objetos estaticos
+            gameDisplay.blit(bgImg2, (bg_x2, bg_y2))#Objetos estaticos
+            car(car_x, car_y)  #Objetos estaticos
+            draw_things(thing_startx, thing_starty, car2Img) #Objetos estaticos
+            highscore(count) #Objetos estaticos
+            pygame.display.update() #Actualiza objetos
+            time.sleep(0.5) #esperar
+            for event in pygame.event.get(): #Si se presiona space reanuda
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         print("Continue...")
-                        pause = False
+                        pygame.mixer.music.unpause() #reanudar sonido
+                        pause = False #Quitar pausa
                         
 
         car_x += car_x_change
 
-        if car_x > road_end_x - car_width:
+        if car_x > road_end_x - car_width: #choque borde de la carretera
             crash(car_x, car_y, count)
-        if car_x < road_start_x:
+        if car_x < road_start_x: #choque borde de la carretera
             crash(car_x - car_width, car_y, count)
 
-        if car_y < thing_starty + thingh - 25:
-            if car_x >= thing_startx + 10 and car_x <= thing_startx + thingw - 10:
+        if car_y < thing_starty + thingh - 25: #choque de frente
+            if car_x >= thing_startx + 10 and car_x <= thing_startx + thingw - 10: #choque de izquierdo con obstaculo
                 # Left Crash
                 crash(car_x - car_width /2, car_y - car_height / 2, count)
-            if car_x + car_width >= thing_startx + 13 and car_x + car_width <= thing_startx + thingw + 10:
+            if car_x + car_width >= thing_startx + 13 and car_x + car_width <= thing_startx + thingw + 10: #choque en la derecha con obstaculo
                 # Right Crash
                 crash(car_x, car_y - car_height / 2, count)
 
-        gameDisplay.blit(bgImg, (bg_x1, bg_y1))
-        gameDisplay.blit(bgImg2, (bg_x2, bg_y2))
+        gameDisplay.blit(bgImg, (bg_x1, bg_y1)) #Actualización Renderizado de fondo1
+        gameDisplay.blit(bgImg2, (bg_x2, bg_y2)) #Actualización Renderizado de fondo2
 
-        car(car_x, car_y)  # display car
-        draw_things(thing_startx, thing_starty, car2Img)
-        highscore(count)
-        count += 1
+        car(car_x, car_y)  #Actualización Auto
+        draw_things(thing_startx, thing_starty, car2Img) #Actualización Renderizado de osbtaculo
+        highscore(count) #Actualización del score
+        count += 1 #Aumenta el score
 
         # Update Speed Obstacle Cars
         thing_speed += 0.001
         thing_starty += thing_speed
 
-        if thing_starty > display_height:
+        if thing_starty > display_height: #Actualización de autos obstaculo
             thing_startx = random.randrange(road_start_x, road_end_x - car_width)
             thing_starty = -200
 
         bg_y1 += bg_speed
         bg_y2 += bg_speed
 
-        if bg_y1 >= display_height:
+        if bg_y1 >= display_height: #Actualización de fondo1
             bg_y1 = -600
 
-        if bg_y2 >= display_height:
+        if bg_y2 >= display_height: #Actualización de fondo2
             bg_y2 = -600
         #camera()
         pygame.display.update()  # update the screen
